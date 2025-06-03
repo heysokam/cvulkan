b866cc56bf4775c748018221332af73e4e0c4f66
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-03 05:24:02 +0200

    new: cvk_result skeleton

---
+++ src/cvulkan/result.h
@@ -0,0 +1,26 @@
+//:________________________________________________________________
+//  cvk  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
+//:________________________________________________________________
+#ifndef H_cvk_result
+#define H_cvk_result
+
+
+#ifndef cvk_result_check
+#include <stdio.h>
+#define cvk_result_check(status, message)                                                      \
+  do {                                                                                         \
+    if (status) printf("[%s:%d][%d] %s: " message "\n", __FILE__, __LINE__, status, __func__); \
+  } while (0)
+#endif  // cvk_result_check
+
+
+#ifndef cvk_assert
+#include <stdio.h>
+#define cvk_assert(condition, message)                                                \
+  do {                                                                                \
+    if (!(condition)) printf("[%s:%d] %s: " message "\n", __FILE__, __LINE__, __func__); \
+  } while (0)
+#endif  // cvk_assert
+
+
+#endif  // H_cvk_result
