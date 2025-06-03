68e824e23303918bac42b4f41a5e6d234b0a6146
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-03 07:55:51 +0200

    new: cvk_Application info creation

---
+++ src/cvulkan/application.c
@@ -0,0 +1,18 @@
+//:________________________________________________________________
+//  cvk  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
+//:________________________________________________________________
+#include "./application.h"
+#include "./config.h"
+
+cvk_Pure cvk_Application cvk_Application_defaults () {
+  return (cvk_Application){
+    .sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+    .pNext              = NULL,
+    .applicationVersion = VK_MAKE_VERSION(0, 0, 0),
+    .pApplicationName   = "cvk | Generic Application",
+    .engineVersion      = VK_MAKE_VERSION(0, 0, 0),
+    .pEngineName        = "cvk | Generic Engine",
+    .apiVersion         = cvk_config_vulkan_version,
+  };
+}
+
---
+++ src/cvulkan/application.h
@@ -0,0 +1,23 @@
+//:________________________________________________________________
+//  cvk  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
+//:________________________________________________________________
+#ifndef H_cvulkan_application
+#define H_cvulkan_application
+#include "./base.h"
+
+#define cvk_Application VkApplicationInfo
+cvk_Pure cvk_Application cvk_Application_defaults ();
+
+
+//______________________________________
+// @section Single Header Support
+//____________________________
+#ifdef cvk_Implementation
+#define cvk_Implementation_application
+#endif
+#ifdef cvk_Implementation_application
+#include "./application.c"
+#endif
+
+#endif  // H_cvulkan_application
+
