55bd4f1eb5db12add3af03579b8e8182e9fab96c
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-03 19:26:42 +0200

    new: Hello Allocator (cpu:stdlib, gpu:null)

--- src/cvulkan/memory.c
+++ src/cvulkan/memory.c
@@ -2,5 +2,51 @@
 //  cvulkan  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
 //:____________________________________________________________________
 #include "./memory.h"
-// TODO:
-cvk_Pure cvk_Allocator cvk_Allocator_defaults () { return (cvk_Allocator){ .gpu = NULL }; }
+#include <stdlib.h>
+
+
+cvk_Pure cvk_Slice cvk_allocator_alloc_stdlib (
+  cvk_Allocator_CPU* const A,
+  cvk_size const           count,
+  cvk_size const           itemsize
+) {
+  cvk_discard(A);
+  return (cvk_Slice){ .len = count, .ptr = malloc(count * itemsize) };
+}
+
+
+cvk_Pure cvk_Slice cvk_allocator_allocZ_stdlib (
+  cvk_Allocator_CPU* const A,
+  cvk_size const           count,
+  cvk_size const           itemsize
+) {
+  cvk_discard(A);
+  return (cvk_Slice){ .len = count, .ptr = calloc(count, itemsize) };
+}
+
+
+void cvk_allocator_free_stdlib (
+  cvk_Allocator_CPU* const A,
+  cvk_Slice                data
+) {
+  cvk_discard(A);
+  free(data.ptr);
+}
+
+
+cvk_Pure cvk_Allocator_CPU cvk_allocator_cpu_stdlib () {
+  return (cvk_Allocator_CPU){
+    .alloc  = cvk_allocator_alloc_stdlib,
+    .allocZ = cvk_allocator_allocZ_stdlib,
+    .free   = cvk_allocator_free_stdlib,
+  };
+}
+
+
+cvk_Pure cvk_Allocator cvk_Allocator_defaults () {
+  return (cvk_Allocator){
+    .gpu = cvk_allocator_gpu_defaults(),
+    .cpu = cvk_allocator_cpu_defaults(),
+  };
+}
+
--- src/cvulkan/memory.h
+++ src/cvulkan/memory.h
@@ -7,14 +7,79 @@
 #define H_cvulkan_memory
 #include "./base.h"
 
-#define cvk_Allocator_CPU void*
-#define cvk_Allocator_GPU VkAllocationCallbacks*
+//______________________________________
+// @section Allocator.CPU: Overridable Type
+//____________________________
+#ifndef cvk_Allocator_CPU
+typedef struct cvk_Allocator_CPU_s cvk_Allocator_CPU_t;
+#define cvk_Allocator_CPU cvk_Allocator_CPU_t
+#endif  // cvk_Allocator_CPU
 
+//______________________________________
+// @section Allocator.CPU: Function Types Declarations
+//____________________________
+typedef cvk_Pure cvk_Slice (*cvk_Fn_allocator_alloc)(cvk_Allocator_CPU* const, cvk_size const, cvk_size const);
+typedef cvk_Pure cvk_Slice (*cvk_Fn_allocator_allocZ)(cvk_Allocator_CPU* const, cvk_size const, cvk_size const);
+typedef void (*cvk_Fn_allocator_free)(cvk_Allocator_CPU* const, cvk_Slice);
+
+
+//______________________________________
+// @section Allocator.CPU: Core Type
+//____________________________
+struct cvk_Allocator_CPU_s {
+  cvk_Fn_allocator_alloc  alloc;
+  cvk_Fn_allocator_allocZ allocZ;
+  cvk_Fn_allocator_free   free;
+};
+
+
+//______________________________________
+// @section Allocator: Core Types
+//____________________________
+#define cvk_Allocator_GPU VkAllocationCallbacks*
 typedef struct cvk_Allocator {
   cvk_Allocator_GPU gpu;
-  cvk_Allocator_CPU cpu;  // TODO:
+  cvk_Allocator_CPU cpu;
 } cvk_Allocator;
 
+
+//______________________________________
+// @section Allocator.CPU: Defaults (stdlib)
+//____________________________
+
+/// @description Thin wrapper for using stdlib.h/malloc with our allocator API
+cvk_Pure cvk_Slice cvk_allocator_alloc_stdlib (cvk_Allocator_CPU* const A, cvk_size const count, cvk_size const itemsize);
+/// @description Thin wrapper for using stdlib.h/calloc with our allocator API
+cvk_Pure cvk_Slice cvk_allocator_allocZ_stdlib (cvk_Allocator_CPU* const A, cvk_size const count, cvk_size const itemsize);
+/// @description Thin wrapper for using stdlib.h/free with our allocator API
+void cvk_allocator_free_stdlib (cvk_Allocator_CPU* const A, cvk_Slice data);
+/// @description
+/// Function that creates a stdlib CPU allocator wrapper
+/// Allows using stdlib.h with our allocator API
+cvk_Pure cvk_Allocator_CPU cvk_allocator_cpu_stdlib ();
+#ifndef cvk_allocator_cpu_defaults
+/// @description
+/// Function that creates the default allocator that cvk will use internally for CPU allocations
+/// @overridable Add `#define cvk_allocator_cpu_defaults yourAllocatorGeneratorFunction` to your code to change this default
+#define cvk_allocator_cpu_defaults() cvk_allocator_cpu_stdlib()
+#endif  // cvk_allocator_cpu_defaults
+
+//______________________________________
+// @section Allocator.GPU: NULL
+//____________________________
+#ifndef cvk_allocator_gpu_defaults
+/// @description
+/// Function that creates the pointer to the default GPU allocator that will be sent to Vulkan
+/// @overridable Add `#define cvk_allocator_gpu_defaults yourAllocatorGeneratorFunction` to your code to change this default behavior
+#define cvk_allocator_gpu_defaults() (VkAllocationCallbacks*)(NULL)
+#endif  // cvk_allocator_gpu_defaults
+
+//______________________________________
+// @section Allocator: Default (cpu:stdlib, gpu:NULL)
+//____________________________
+
+/// @description
+/// Function that creates the default `cvk_Allocator` object used by the cvk API
 cvk_Pure cvk_Allocator cvk_Allocator_defaults ();
 
 
@@ -30,3 +95,4 @@ cvk_Pure cvk_Allocator cvk_Allocator_defaults ();
 
 
 #endif  // H_cvulkan_memory
+
