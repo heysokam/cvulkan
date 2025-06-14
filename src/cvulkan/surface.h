38289a9f82694f32c6fc5125aea2530687d01182
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-14 11:01:11 +0200

    new: Surface alias

---
+++ src/cvulkan/surface.h
@@ -0,0 +1,15 @@
+//:____________________________________________________________________
+//  cvulkan  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
+//:____________________________________________________________________
+//! @fileoverview Cable connector to the public modules of the library.
+//_____________________________________________________________________|
+#ifndef H_cvulkan_surface
+#define H_cvulkan_surface
+#include "./base.h"
+
+/// @description
+/// Alias to unify the naming convention of cvulkan types
+typedef VkSurfaceKHR cvk_Surface;
+
+#endif  // H_cvulkan_surface
+
