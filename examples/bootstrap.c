c1f5905be8fcd0d5fc99464c4ab54fc2b845b149
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-15 00:38:28 +0200

    doc: File skeleton for the boilerplate bootstrapping example

---
+++ examples/bootstrap.c
@@ -0,0 +1,17 @@
+//:____________________________________________________________________
+//  cvulkan  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
+//:____________________________________________________________________
+//! @fileoverview
+//! Example of using cvulkan
+//! to bootstrap all Vulkan boilerplate
+//! up to the point of retrieving a `VkSwapchainKHR` handle.
+//!
+//! The functions used in this pure C example
+//! provide the same functionality that `vk-bootstrap` does for C++
+//! https://github.com/charles-lunarg/vk-bootstrap
+//!
+//! @note
+//! Instead of using the Builder Pattern, like vk-bootstrap does,
+//! cvulkan uses vanilla C patterns and tools to achieve the same result.
+//________________________________________________________________________|
+// TODO: Current ./wip.c will become this file
