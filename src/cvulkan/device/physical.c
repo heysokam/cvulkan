6c7e4f468beb20fd9c9be5ad6f3ef148b5f79ea8
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-15 09:45:50 +0200

    fmt: Change `device/physical.c` to the new allocator API

--- src/cvulkan/device/physical.c
+++ src/cvulkan/device/physical.c
@@ -11,24 +11,38 @@ cvk_Pure cvk_bool cvk_device_physical_isSuitable_default (
   cvk_Allocator* const             allocator
 ) {
   cvk_bool result = cvk_false;
-  // Allocate the list of QueueFamilies of this device
-  cvk_QueueFamilies queueFamilies = cvk_queue_families_create(device, surface, allocator);
+  // Allocate the QueueFamilies, Device.Extensions, Properties and Swapchain support of this device
+  cvk_QueueFamilies          queueFamilies = cvk_queue_families_create(device, surface, allocator);
+  cvk_device_Extensions      extensions    = cvk_device_Extensions_default(allocator);
+  VkPhysicalDeviceProperties properties    = (VkPhysicalDeviceProperties){ 0 };
+  vkGetPhysicalDeviceProperties(device->ct, &properties);
   // Find if the device is suitable
-  if (cvk_Optional_u32_hasValue(queueFamilies.graphics)) result = cvk_true;
-  // Cleanup the QueueFamilies list we just allocated
+  result =  // clang-format off
+    properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU
+    && cvk_Optional_u32_hasValue(queueFamilies.graphics)
+    && cvk_Optional_u32_hasValue(queueFamilies.present)
+    && cvk_device_extensions_supported(device, extensions, allocator)
+    ;  // clang-format on
+  // Cleanup the lists we just allocated
+  allocator->cpu.free(&allocator->cpu, (cvk_Slice*)&extensions);
   cvk_queue_families_destroy(&queueFamilies, allocator);
   return result;
 }  //:: cvk_device_physical_isSuitable_default
 
-
-cvk_Pure cvk_Slice /* VkPhysicalDevice[] */ cvk_device_physical_getAvailable (
+cvk_Pure cvk_device_physical_List cvk_device_physical_getAvailable (
   cvk_Instance* const instance
 ) {
-  cvk_Slice result = cvk_Slice_empty();
-  cvk_result_check(vkEnumeratePhysicalDevices(instance->ct, (uint32_t*)&result.len, NULL), "Failed when searching for GPUs with Vulkan support.");
+  cvk_device_physical_List result = (cvk_device_physical_List){ 0 };
+  // clang-format off
+  cvk_result_check(vkEnumeratePhysicalDevices(instance->ct, (uint32_t*)&result.len, NULL),
+    "Failed when searching for GPUs with Vulkan support.");
   cvk_assert(result.len, "Failed to find any GPUs with Vulkan support.");
-  result = /* VkPhysicalDevice[] */ instance->allocator.cpu.allocZ(&instance->allocator.cpu, result.len, sizeof(VkPhysicalDevice));
-  cvk_result_check(vkEnumeratePhysicalDevices(instance->ct, (uint32_t*)&result.len, result.ptr), "Failed to retrieve the list of GPUs.");
+  if (result.len) {
+    cvk_Slice data = instance->allocator.cpu.allocZ(&instance->allocator.cpu, result.len, sizeof(VkPhysicalDevice));
+    cvk_result_check(vkEnumeratePhysicalDevices(instance->ct, (uint32_t*)&data.len, data.ptr), "Failed to retrieve the list of GPUs.");
+    result.len = data.len;
+    result.ptr = (VkPhysicalDevice*)data.ptr;
+  } // clang-format on
   return result;
 }  //:: cvk_device_physical_getAvailable
 
@@ -37,7 +51,7 @@ cvk_Pure cvk_device_Physical cvk_device_physical_create (
   cvk_device_physical_create_args* const arg
 ) {
   // Get the list of available devices
-  cvk_Slice available = cvk_device_physical_getAvailable(arg->instance);
+  cvk_device_physical_List available = cvk_device_physical_getAvailable(arg->instance);
   // Get the isSuitable function (user or default)
   cvk_Fn_device_physical_isSuitable const isSuitable = (!arg->isSuitable) ? arg->isSuitable : cvk_device_physical_isSuitable;
   // For each available device, call isSuitable and stop when the condition matches
@@ -45,7 +59,7 @@ cvk_Pure cvk_device_Physical cvk_device_physical_create (
   for (cvk_Optional_u32 id = 0; id < available.len; ++id) {  // clang-format off
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wunsafe-buffer-usage"
-    result = (cvk_device_Physical){ .ct = ((VkPhysicalDevice*)available.ptr)[id], .id = id };
+    result = (cvk_device_Physical){ .ct = available.ptr[id], .id = id };
     #pragma GCC diagnostic pop  // -Wunsafe-buffer-usage
     if (arg->forceFirst) break;  // clang-format on
     // Continue/stop based on suitability
@@ -55,7 +69,7 @@ cvk_Pure cvk_device_Physical cvk_device_physical_create (
     }
   }
   // Cleanup the list of devices
-  arg->instance->allocator.cpu.free(&arg->instance->allocator.cpu, available);
+  arg->instance->allocator.cpu.free(&arg->instance->allocator.cpu, (cvk_Slice*)&available);
   // Validate and Return the result
   cvk_assert(cvk_Optional_u32_hasValue(result.id), "Failed to find a Physical Device (GPU) suitable for Vulkan.");
   cvk_assert(result.ct != VK_NULL_HANDLE, "Failed to find a Physical Device (GPU) suitable for Vulkan.");
