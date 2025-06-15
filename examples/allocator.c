f3b8d5e023593cb12bff8273225afb4ed63d8584
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-15 09:43:43 +0200

    doc: Minimal Allocator.cpu example

---
+++ examples/allocator.c
@@ -0,0 +1,24 @@
+//:____________________________________________________________________
+//  cvulkan  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
+//:____________________________________________________________________
+//! @fileoverview
+//! Example of how to use the cvulkan Allocator API
+//__________________________________________________|
+#include <assert.h>
+#include <cvulkan/memory.h>
+
+int main () {
+  cvk_Allocator std = cvk_Allocator_defaults();
+
+  cvk_Slice result = std.cpu.allocZ(&std.cpu, 42, sizeof(char));
+  for (cvk_size id = 0; id < result.len; ++id) {
+    char* list = (char*)result.ptr;  // Slices are unaware of their type
+    assert(list[id] == 0);
+  }
+
+  size_t len = result.len;
+  std.cpu.free(&std.cpu, &result);
+
+  return len;
+}
+
