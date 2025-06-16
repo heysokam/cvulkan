27ec58938a155ae0fb0d2e7c20c5aeca2a8c6777
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-16 02:23:40 +0200

    doc: Copy the Bootstrap example code to its file

--- examples/bootstrap.c
+++ examples/bootstrap.c
@@ -2,9 +2,8 @@
 //  cvulkan  |  Copyright (C) Ivan Mar (sOkam!)  |  LGPLv3 or higher  :
 //:____________________________________________________________________
 //! @fileoverview
-//! Example of using cvulkan
-//! to bootstrap all Vulkan boilerplate
-//! up to the point of retrieving a `VkSwapchainKHR` handle.
+//! Example of using cvulkan to bootstrap all boilerplate
+//! up to retrieving a `VkSwapchainKHR` handle.
 //!
 //! The functions used in this pure C example
 //! provide the same functionality that `vk-bootstrap` does for C++
@@ -14,4 +13,102 @@
 //! Instead of using the Builder Pattern, like vk-bootstrap does,
 //! cvulkan uses vanilla C patterns and tools to achieve the same result.
 //________________________________________________________________________|
-// TODO: Current ./wip.c will become this file
+// @deps cdk
+#define GLFW_INCLUDE_VULKAN
+#define csys_Implementation
+#include <csys.h>
+// @deps cvulkan
+#define cvk_Implementation
+#include <cvulkan.h>
+
+
+typedef struct Bootstrap {
+  cvk_Instance         instance;
+  cvk_Surface          surface;
+  cvk_device_Physical  device_physical;
+  cvk_device_Queue     device_queue;
+  cvk_device_Logical   device_logical;
+  cvk_device_Swapchain device_swapchain;
+} Bootstrap;
+
+
+/// @important
+/// Surface requesting would force this library to depend on a specific windowing library.
+/// As such, cvulkan expects you to send the VkSurfaceKHR handle as input.
+/// How you request that handle is up to you. cvulkan doesn't need to know about it.
+///
+/// @description
+/// Example of how to create a valid Vulkan Surface for the given GLFW {@arg window}
+///
+static cvk_Surface bootstrap_surface_create (
+  VkInstance const       instance,
+  GLFWwindow* const      window,
+  VkAllocationCallbacks* allocator
+) {
+  VkSurfaceKHR result = NULL;  // clang-format off
+  cvk_result_check(glfwCreateWindowSurface(instance, window, allocator, &result),
+    "Failed to create the Vulkan Surface for the given GLFW window.");  // clang-format on
+  return result;
+}
+#define bootstrap_surface_destroy vkDestroySurfaceKHR
+
+
+static Bootstrap bootstrap_create (
+  GLFWwindow* const window_ct,  ///< Only for surface creation. @see bootstrap_surface_create for more info
+  cvk_Size2D const  window_size
+) {  // clang-format off
+  Bootstrap result        = (Bootstrap){ 0 };
+  result.instance         = cvk_instance_create(&(cvk_instance_create_args){ 0 });  // Create with all defaults
+  result.surface          = bootstrap_surface_create(result.instance.ct, window_ct, result.instance.allocator.gpu);
+  result.device_physical  = cvk_device_physical_create(&(cvk_device_physical_create_args){
+    .instance             = &result.instance,
+    .surface              = result.surface,
+    .forceFirst           = cvk_true,
+  });
+  result.device_queue     = cvk_device_queue_create_noContext(&(cvk_device_queue_create_args){
+    .instance             = &result.instance,
+    .device               = &result.device_physical,
+    .id                   = result.device_physical.queueFamilies.graphics,
+    .priority             = 1.0f,
+  });
+  result.device_logical   = cvk_device_logical_create(&(cvk_device_logical_create_args){
+    .physical             = &result.device_physical,
+    .queue                = &result.device_queue,
+    .allocator            = &result.instance.allocator,
+  });
+  cvk_device_queue_create_context(&result.device_queue, &result.device_logical);
+  result.device_swapchain = cvk_device_swapchain_create(&(cvk_device_swapchain_create_args){
+    .device_physical      = &result.device_physical,
+    .device_logical       = &result.device_logical,
+    .surface              = result.surface,
+    .size                 = window_size,
+    .allocator            = &result.instance.allocator,
+  });  // clang-format on
+  return result;
+}
+
+
+static void bootstrap_destroy (
+  Bootstrap* gpu
+) {
+  cvk_device_swapchain_destroy(&gpu->device_swapchain, &gpu->device_logical, &gpu->instance.allocator);
+  cvk_device_logical_destroy(&gpu->device_logical, &gpu->instance.allocator);
+  cvk_device_queue_destroy(&gpu->device_queue, &gpu->instance.allocator);
+  cvk_device_physical_destroy(&gpu->device_physical, &gpu->instance.allocator);
+  bootstrap_surface_destroy(gpu->instance.ct, gpu->surface, gpu->instance.allocator.gpu);
+  cvk_instance_destroy(&gpu->instance);
+}
+
+
+int main () {
+  // Initialize
+  csys_System sys = csys_init(csys_init_Options_defaults());
+  Bootstrap   gpu = bootstrap_create(sys.window.ct, (cvk_Size2D){ .width = sys.window.width, .height = sys.window.height });
+  // Update Loop
+  while (!csys_close(&sys)) { csys_update(&sys); }
+  // Terminate
+  bootstrap_destroy(&gpu);
+  csys_term(&sys);
+  return 0;
+}
+
--- examples/wip.c
+++ examples/wip.c
@@ -17,7 +17,7 @@ typedef struct Example {
   cvk_device_Queue     device_queue;
   cvk_device_Logical   device_logical;
   cvk_device_Swapchain device_swapchain;
-} Bootstrap;
+} Example;
 
 
 /// @important
@@ -28,7 +28,7 @@ typedef struct Example {
 /// @description
 /// Example of how to create a valid Vulkan Surface for the given GLFW {@arg window}
 ///
-static cvk_Surface bootstrap_surface_create (
+static cvk_Surface example_surface_create (
   VkInstance const       instance,
   GLFWwindow* const      window,
   VkAllocationCallbacks* allocator
@@ -38,52 +38,52 @@ static cvk_Surface bootstrap_surface_create (
     "Failed to create the Vulkan Surface for the given GLFW window.");  // clang-format on
   return result;
 }
-#define bootstrap_surface_destroy vkDestroySurfaceKHR
+#define example_surface_destroy vkDestroySurfaceKHR
 
 
-static Bootstrap bootstrap_create (
-  GLFWwindow* const window_ct,  ///< Only for surface creation. @see bootstrap_surface_create for more info
+static Example example_create (
+  GLFWwindow* const window_ct,  ///< Only for surface creation. @see example_surface_create for more info
   cvk_Size2D const  window_size
-) {
-  Bootstrap result       = (Bootstrap){ 0 };
-  result.instance        = cvk_instance_create(&(cvk_instance_create_args){ 0 });  // Create with all defaults
-  result.surface         = bootstrap_surface_create(result.instance.ct, window_ct, result.instance.allocator.gpu);
-  result.device_physical = cvk_device_physical_create(&(cvk_device_physical_create_args){
-    .instance   = &result.instance,
-    .surface    = result.surface,
-    .forceFirst = cvk_true,
-  });  // clang-format off
-  result.device_queue = cvk_device_queue_create_noContext(&(cvk_device_queue_create_args){
-    .instance = &result.instance,
-    .device   = &result.device_physical,
-    .id       = result.device_physical.queueFamilies.graphics,
-    .priority = 1.0f,
+) {  // clang-format off
+  Example result          = (Example){ 0 };
+  result.instance         = cvk_instance_create(&(cvk_instance_create_args){ 0 });  // Create with all defaults
+  result.surface          = example_surface_create(result.instance.ct, window_ct, result.instance.allocator.gpu);
+  result.device_physical  = cvk_device_physical_create(&(cvk_device_physical_create_args){
+    .instance             = &result.instance,
+    .surface              = result.surface,
+    .forceFirst           = cvk_true,
   });
-  result.device_logical = cvk_device_logical_create(&(cvk_device_logical_create_args){
-    .physical  = &result.device_physical,
-    .queue     = &result.device_queue,
-    .allocator = &result.instance.allocator,
-  });  // clang-format on
+  result.device_queue     = cvk_device_queue_create_noContext(&(cvk_device_queue_create_args){
+    .instance             = &result.instance,
+    .device               = &result.device_physical,
+    .id                   = result.device_physical.queueFamilies.graphics,
+    .priority             = 1.0f,
+  });
+  result.device_logical   = cvk_device_logical_create(&(cvk_device_logical_create_args){
+    .physical             = &result.device_physical,
+    .queue                = &result.device_queue,
+    .allocator            = &result.instance.allocator,
+  });
   cvk_device_queue_create_context(&result.device_queue, &result.device_logical);
   result.device_swapchain = cvk_device_swapchain_create(&(cvk_device_swapchain_create_args){
-    .device_physical = &result.device_physical,
-    .device_logical  = &result.device_logical,
-    .surface         = result.surface,
-    .size            = window_size,
-    .allocator       = &result.instance.allocator,
-  });
+    .device_physical      = &result.device_physical,
+    .device_logical       = &result.device_logical,
+    .surface              = result.surface,
+    .size                 = window_size,
+    .allocator            = &result.instance.allocator,
+  });  // clang-format on
   return result;
 }
 
 
-static void bootstrap_destroy (
-  Bootstrap* gpu
+static void example_destroy (
+  Example* gpu
 ) {
   cvk_device_swapchain_destroy(&gpu->device_swapchain, &gpu->device_logical, &gpu->instance.allocator);
   cvk_device_logical_destroy(&gpu->device_logical, &gpu->instance.allocator);
   cvk_device_queue_destroy(&gpu->device_queue, &gpu->instance.allocator);
   cvk_device_physical_destroy(&gpu->device_physical, &gpu->instance.allocator);
-  bootstrap_surface_destroy(gpu->instance.ct, gpu->surface, gpu->instance.allocator.gpu);
+  example_surface_destroy(gpu->instance.ct, gpu->surface, gpu->instance.allocator.gpu);
   cvk_instance_destroy(&gpu->instance);
 }
 
@@ -91,11 +91,11 @@ static void bootstrap_destroy (
 int main () {
   // Initialize
   csys_System sys = csys_init(csys_init_Options_defaults());
-  Bootstrap   gpu = bootstrap_create(sys.window.ct, (cvk_Size2D){ .width = sys.window.width, .height = sys.window.height });
+  Example     gpu = example_create(sys.window.ct, (cvk_Size2D){ .width = sys.window.width, .height = sys.window.height });
   // Update Loop
   while (!csys_close(&sys)) { csys_update(&sys); }
   // Terminate
-  bootstrap_destroy(&gpu);
+  example_destroy(&gpu);
   csys_term(&sys);
   return 0;
 }
