436ab30fdbc12c5eaf3684feb7afab27a86fb592
sOkam <7308253+heysokam@users.noreply.github.com>
2025-06-17 14:38:11 +0200

    chg: Rename `RenderTarget` back to `Renderpass`
    The RenderTarget concept is better, and would fit an abstraction really
    well.
    But risks making the cvulkan helpers unintuitive to Vulkan users.

--- src/cvulkan/renderpass.h
+++ src/cvulkan/renderpass.h
@@ -1,14 +1,7 @@
 //:__________________________________________________________
 //  cvulkan  |  Copyright (C) Ivan Mar (sOkam!)  |  MPL-2.0 :
 //:__________________________________________________________
-//! @fileoverview Vulkan RenderTarget: Types & Tools
-//!
-//! @note
-//!  RenderTarget is a very thin wrapper/abstraction over:
-//!  RenderPass + FrameBuffers + Subpasses
-//!  cvk.RenderTarget -> vk.Renderpass
-//!  cvk.RenderPass   -> vk.Subpass
-//!  cvk.Framebuffer  -> vk.Framebuffer
+//! @fileoverview Vulkan Renderpass: Types & Tools
 //!
 //! @important
 //!  cvulkan prioritizes Dynamic Rendering and vulkan 1.3+,
@@ -18,8 +11,8 @@
 //!  Please reach out and open an issue on GitHub
 //!  if there is something missing in this section that you need.
 //____________________________________________________________________|
-#ifndef H_cvulkan_renderTarget
-#define H_cvulkan_renderTarget
+#ifndef H_cvulkan_renderpass
+#define H_cvulkan_renderpass
 #include "./base.h"
 
 
@@ -27,13 +20,14 @@
 // @section Single Header Support
 //____________________________
 #ifdef cvk_Implementation
-#define cvk_Implementation_renderTarget
+#define cvk_Implementation_renderpass
 #endif
-#ifdef cvk_Implementation_renderTarget
-#include "./renderTarget/renderpass.c"
-#include "./renderTarget/framebuffer.c"
+#ifdef cvk_Implementation_renderpass
+#include "./renderpass/pass.c"
+#include "./renderpass/subpass.c"
+#include "./renderpass/framebuffer.c"
 #endif
 
 
-#endif  // H_cvulkan_renderTarget
+#endif  // H_cvulkan_renderpass
 
