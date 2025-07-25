148bc6634b8b693c622d86c2d83849437b9ecaa8
sOkam <7308253+heysokam@users.noreply.github.com>
2025-07-25 20:56:26 +0200

    doc: Minimal Mat4 type for the examples

--- examples/helpers/math.c
+++ examples/helpers/math.c
@@ -9,7 +9,7 @@ typedef double f64;
 // clang-format off
 typedef struct Vec2 { union {
   struct { f32 x,y; };
-  f32 arr[2];
+  f32 v[2];
 };} Vec2;
 // clang-format on
 
@@ -17,8 +17,19 @@ typedef struct Vec2 { union {
 typedef struct Vec3 { union {
   struct { f32 x,y,z; };
   struct { f32 r,g,b; };
-  f32 arr[3];
+  f32 v[3];
 };} Vec3;
+// clang-format on
+
+// clang-format off
+typedef struct Mat4 { union {
+  struct { f32
+    m00,m01,m02,m03,
+    m10,m11,m12,m13,
+    m20,m21,m22,m23,
+    m30,m31,m32,m33; };
+  f32 m[16];
+};} Mat4;
 // clang-format on
 
 typedef struct Vertex {
