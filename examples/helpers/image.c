d073eeda18943ea6838db643b3efef27cbac1308
sOkam <7308253+heysokam@users.noreply.github.com>
2025-07-30 00:02:30 +0200

    doc: Add `stb_image.h` to the WIP example

---
+++ examples/helpers/image.c
@@ -0,0 +1,18 @@
+//:___________________________________________________________
+//  cvulkan  |  Copyright (C) Ivan Mar (sOkam!)  |  CC0-1.0  :
+//:___________________________________________________________
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wpadded"
+#pragma GCC diagnostic ignored "-Wdisabled-macro-expansion"
+#pragma GCC diagnostic ignored "-Wcast-align"
+#pragma GCC diagnostic ignored "-Wdouble-promotion"
+#pragma GCC diagnostic ignored "-Wunsafe-buffer-usage"
+#pragma GCC diagnostic ignored "-Wextra-semi-stmt"
+#pragma GCC diagnostic ignored "-Wswitch-default"
+#pragma GCC diagnostic ignored "-Wimplicit-int-conversion"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#pragma GCC diagnostic pop  // -Wcast-qual -Wsign-conversion -Wpadded -Wdisabled-macro-expansion Wcast-align -Wdouble-promotion -Wunsafe-buffer-usage -Wextra-semi-stmt -Wswitch-default -Wimplicit-int-conversion -Wimplicit-fallthrough
