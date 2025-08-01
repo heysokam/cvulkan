27e9974235bad0aaef0234cf2b3ad9fff4054a92
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-02 01:10:50 +0200

    doc: Change `linmath` to `cglm`

--- examples/helpers/math.c
+++ examples/helpers/math.c
@@ -3,6 +3,7 @@
 //:___________________________________________________________
 //! @fileoverview Math helper code for the examples
 //__________________________________________________|
+/*
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdouble-promotion"
 #pragma GCC diagnostic ignored "-Wimplicit-float-conversion"
@@ -13,16 +14,41 @@
 typedef vec2   Vec2;
 typedef vec3   Vec3;
 typedef mat4x4 Mat4;
+*/
 
-typedef enum AxisID { // clang-format off
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wreserved-macro-identifier"
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+#pragma GCC diagnostic ignored "-Wpre-c23-compat"
+#pragma GCC diagnostic ignored "-Wdocumentation"
+#pragma GCC diagnostic ignored "-Wdocumentation-pedantic"
+#pragma GCC diagnostic ignored "-Wextra-semi-stmt"
+#pragma GCC diagnostic ignored "-Wswitch-default"
+#pragma GCC diagnostic ignored "-Wbad-function-cast"
+#pragma GCC diagnostic ignored "-Wunsafe-buffer-usage"
+#define CGLM_FORCE_DEPTH_ZERO_TO_ONE  // Required for vulkan. Not default
+// #define CGLM_FORCE_LEFT_HANDED        // Required for vulkan. Not default
+#include <cglm.h>
+#pragma GCC diagnostic pop  // -Wreserved-macro-identifier -Wfloat-equal -Wpre-c23-compat -Wdocumentation -Wdocumentation-pedantic -Wextra-semi-stmt -Wswitch-default -Wbad-function-cast -Wunsafe-buffer-usage
+
+typedef vec2 Vec2;
+typedef vec3 Vec3;
+typedef mat4 Mat4;
+#define mat4x4_identity(m)                              glm_mat4_identity(m)
+#define mat4x4_look_at(m, eye, center, up)              glm_lookat(eye, center, up, m)
+#define mat4x4_perspective(m, y_fov, aspect, near, far) glm_perspective(y_fov, aspect, near, far, m)
+#define mat4x4_rotate_Z(m, Q, angle)                    glm_rotate_z(Q, angle, m)
+
+
+typedef enum AxisID {  // clang-format off
   X, Y, Z, W,
   R= X, G= Y, B= Z, A= W,
   U= X, V= Y
-} AxisID; // clang-format on
+} AxisID;  // clang-format on
 
 typedef struct Vertex {
-  Vec2 pos;
   Vec2 uv;
+  Vec3 pos;
   Vec3 color;
 } Vertex;
 
