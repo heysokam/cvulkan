a428796629725fc6ef895801c71ec723d14bfdcb
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-04 16:37:40 +0200

    bld: Add `tinyobjloader-c` to the examples buidsystem

--- build.nim
+++ build.nim
@@ -127,6 +127,7 @@ template example_shaders_compile=
 #_____________________________
 const cglm = Dependency.new("cglm", "https://github.com/recp/cglm",             "include/cglm")
 const stb  = Dependency.new("stb",  "https://github.com/nothings/stb",          "")
+const obj  = Dependency.new("obj",  "https://github.com/syoyo/tinyobjloader-c", "")
 
 
 #_______________________________________
@@ -139,7 +140,7 @@ const wip_flags = Flags(
   cc: examples_flags.cc & @["-Wunsafe-buffer-usage"],
   ld: examples_flags.ld)
 confy.cfg.dirs.src = dir_examples
-let example_wip = Program.new("wip.c",           flags=      wip_flags, deps= @[cglm, stb])
+let example_wip = Program.new("wip.c",           flags=      wip_flags, deps= @[cglm, stb, obj])
 let example_001 = Program.new("001.instance.c",  flags= examples_flags)
 let example_002 = Program.new("002.bootstrap.c", flags= examples_flags)
 let example_003 = Program.new("003.triangle.c",  flags= examples_flags)
